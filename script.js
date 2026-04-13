699a81774387c4073bf9aaa2cb84f1b1a83b6ce3
Mohammad Ishan Ahmad <mdishanahmad@gmail.com>
2026-04-13 17:44:05 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,32 @@
-//your JS code here. If required.
+let codesEl=document.querySelectorAll(".code");
+
+codesEl.forEach((code,index)=>
+	{
+	code.addEventListener("input",()=>
+			{
+				if(!/^[0-9]$/.test(code.value))
+		{
+			code.value="";
+			return;
+		}
+				if(code.value.length===1)
+						{
+							if(index<codesEl.length-1)
+							{
+								codesEl[index+1].focus();
+							}
+						}
+				
+			});
+
+		code.addEventListener("keydown",(e)=>
+			{
+				if(e.key=="Backspace")
+				{
+					if(code.value=="" && index>0 )
+					{
+						codesEl[index-1].focus();
+					}
+				}
+			})
+	})
